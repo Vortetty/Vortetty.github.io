31d7454043d4d2605258fb24afc47deeea6befa9
Vortetty <33466216+Vortetty@users.noreply.github.com>
2019-08-30 10:07:20 -0700

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,126 +1,60 @@
-var inputReady = true;
-var input = $('.404-input');
-input.focus();
-$('.container').on('click', function(e){
-  input.focus();
-});
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
 
-input.on('keyup', function(e){
-  $('.new-output').text(input.val());
-  // console.log(inputReady);
-});
+var noise = (function() {
+  var ب_ب;
+  var ಥ_ಥ;
+  var imgData;
+  var px;
+  var w;
+  var h;
+  var static;
 
-$('.four-oh-four-form').on('submit', function(e){
-  e.preventDefault();
-  var val = $(this).children($('.404-input')).val().toLowerCase();
-  var href;
+  var flicker = function() {
+    ب_ب = document.getElementById('canv');
+    ಥ_ಥ = ب_ب.getContext('2d');
+    ب_ب.width = w = 700;
+    ب_ب.height = h = 500;
+    ಥ_ಥ.fillStyle = 'hsla(255,255%,255%,1)';
+    ಥ_ಥ.fillRect(0, 0, w, h);
+    ಥ_ಥ.fill();
+    imgData = ಥ_ಥ.getImageData(0, 0, w, h);
+    px = imgData.data;
+    static();
+  };
 
-	 if (val === 'kittens'){
-    showKittens();
-  }else {
-    resetForm();
+  static = function() {
+    setInterval(function() {
+      window.requestAnimFrame(static);
+      flicks();
+    }, 30);
   }
-});
+  var flicks = function() {
+    for (var i = 0; i < px.length; i += 4) {
+      var col = (Math.random() * 255) + 50;
+      px[i] = col;
+      px[i + 1] = col;
+      px[i + 2] = col;
+    }
+    ಥ_ಥ.putImageData(imgData, 0, 0);
+  };
 
-function resetForm(withKittens){
-  var message = "Sorry that command is not recognized."
-  var input = $('.404-input');
+  return {
+    init: flicker
+  };
+}());
 
-  if (withKittens){
-    $('.kittens').removeClass('kittens');
-    message = "Huzzzzzah Kittehs!"
-  }
+noise.init();
 
-  $('.new-output').removeClass('new-output');
-  input.val('');
-  $('.terminal').append('<p class="prompt">' + message + '</p><p class="prompt output new-output"></p>');
-
-  $('.new-output').velocity(
-    'scroll'
-  ), {duration: 100}
-}
-
-	function showKittens(){
-		$('.terminal').append("<div class='kittens'>"+
-								 "<p class='prompt'>	                             ,----,         ,----,                                          ,---,</p>" +
-								 "<p class='prompt'>       ,--.                ,/   .`|       ,/   .`|                     ,--.              ,`--.' |</p>" +
-								 "<p class='prompt'>   ,--/  /|    ,---,     ,`   .'  :     ,`   .'  :     ,---,.        ,--.'|   .--.--.    |   :  :</p>" +
-								 "<p class='prompt'>,---,': / ' ,`--.' |   ;    ;     /   ;    ;     /   ,'  .' |    ,--,:  : |  /  /    '.  '   '  ;</p>" +
-								 "<p class='prompt'>:   : '/ /  |   :  : .'___,/    ,'  .'___,/    ,'  ,---.'   | ,`--.'`|  ' : |  :  /`. /  |   |  |</p>" +
-								 "<p class='prompt'>|   '   ,   :   |  ' |    :     |   |    :     |   |   |   .' |   :  :  | | ;  |  |--`   '   :  ;</p>" +
-								 "<p class='prompt'>'   |  /    |   :  | ;    |.';  ;   ;    |.';  ;   :   :  |-, :   |   \\ | : |  :  ;_     |   |  '</p>" +
-								 "<p class='prompt'>|   ;  ;    '   '  ; `----'  |  |   `----'  |  |   :   |  ;/| |   : '  '; |  \\  \\    `.  '   :  |</p>" +
-								 "<p class='prompt'>:   '   \\   |   |  |     '   :  ;       '   :  ;   |   :   .' '   ' ;.    ;   `----.   \\ ;   |  ;</p>" +
-								 "<p class='prompt'>'   : |.  \\ |   |  '     '   :  |       '   :  |   '   :  ;/| '   : |  ; .'  /  /`--'  /  `--..`;  </p>" +
-								 "<p class='prompt'>|   | '_\\.' '   :  |     ;   |.'        ;   |.'    |   |    \\ |   | '`--'   '--'.     /  .--,_   </p>" +
-								 "<p class='prompt'>'   : |     ;   |.'      '---'          '---'      |   :   .' '   : |         `--'---'   |    |`.  </p>" +
-								 "<p class='prompt'>;   |,'     '---'                                  |   | ,'   ;   |.'                    `-- -`, ; </p>" +
-								 "<p class='prompt'>'---'                                              `----'     '---'                        '---`'</p>" +
-								 "<p class='prompt'>                                                              </p></div>");
-
-		
-		var lines = $('.kittens p');
-		$.each(lines, function(index, line){
-			setTimeout(function(){
-				$(line).css({
-					"opacity": 1
-				});
-
-				textEffect($(line))
-			}, index * 100);
-		});
-
-		$('.new-output').velocity(
-			'scroll'
-		), {duration: 100}
-
-		setTimeout(function(){
-			var gif;
-
-			$.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kittens', function(result){
-				gif = result.data.image_url;
-				$('.terminal').append('<img class="kitten-gif" src="' + gif + '"">');
-				resetForm(true);
-			});
-		}, (lines.length * 100) + 1000);
-	}
-
-	function textEffect(line){
-		var alpha = [';', '.', ',', ':', ';', '~', '`'];
-		var animationSpeed = 10;
-		var index = 0;
-		var string = line.text();
-		var splitString = string.split("");
-		var copyString = splitString.slice(0);
-
-		var emptyString = copyString.map(function(el){
-		    return [alpha[Math.floor(Math.random() * (alpha.length))], index++];
-		})
-
-		emptyString = shuffle(emptyString);
-
-		$.each(copyString, function(i, el){
-		    var newChar = emptyString[i];
-		    toUnderscore(copyString, line, newChar);
-
-		    setTimeout(function(){
-		      fromUnderscore(copyString, splitString, newChar, line);
-		    },i * animationSpeed);
-		  })
-	}
-
-	function toUnderscore(copyString, line, newChar){
-		copyString[newChar[1]] = newChar[0];
-		line.text(copyString.join(''));
-	}
-
-	function fromUnderscore(copyString, splitString, newChar, line){
-		copyString[newChar[1]] = splitString[newChar[1]];
-		line.text(copyString.join(""));
-	}
-
-
-	function shuffle(o){
-	    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	    return o;
-	};
+ function updatepage() {
+            document.getElementById("page").innerHTML = window.location.href;window.fitText( document.getElementById("page"), 1.5);
+        }
+        window.onload = updatepage;
